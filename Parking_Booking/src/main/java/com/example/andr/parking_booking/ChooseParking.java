7afa33d8baaf8342f13409d84ca04bc534175492
Anagha M Anil Kumar <43451020+Anagha18@users.noreply.github.com>
2020-09-30 03:45:46 +0530

    Initial Version Parking Booking

---
+++ Parking_Booking/src/main/java/com/example/andr/parking_booking/ChooseParking.java
@@ -0,0 +1,21 @@
+package com.example.andr.parking_booking;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseParking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_parking);
+    }
+    public void pesu(View v)
+    {
+        Intent i=new Intent(this, TypeVehicle.class);
+        startActivity(i);
+
+    }
+}
---
+++ Parking_Booking/src/main/java/com/example/andr/parking_booking/TypeVehicle.java
@@ -0,0 +1,13 @@
+package com.example.andr.parking_booking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TypeVehicle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_vehicle);
+    }
+}
